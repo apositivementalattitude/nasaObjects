0cb18663bea10b37ebb8a9768a461ef1033cea97
Eric Lee <eazylee92@gmail.com>
2023-02-16 00:16:40 -0800

    added styling to the button, hid the text input, added background coloring, and changed the picture sizing

--- objects-NASA/js/main.js
+++ objects-NASA/js/main.js
@@ -1,4 +1,4 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+//The user will enter a date. Get a space name and photo, and place them in the DOM
 document.querySelector('button').addEventListener('click', getPics)
 
 function getPics(){
